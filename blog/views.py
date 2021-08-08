b6f8b745bfafe7312d0aeb9b435d3807911bdb3e
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-08-08 19:47:32 +0500

    some css added, and maded test.py file ready

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,3 +7,8 @@ from .models import Post
 class BlogListView(ListView):
 	model = Post
 	template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+	model = Post
+	template_name = 'post_detail.html'
