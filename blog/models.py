325304e133cfeaf54ac578579fd648e86e68a411
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-08-21 14:19:13 +0500

    sign up and login website is added

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -13,3 +14,6 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post_detail', args=[str(self.pk)])
